ba40ca4f269ce869c8e9cfc6fb0f916b684c464d
eliaelbaz <eliaelbaz050@gmail.com>
2024-09-17 19:49:43 +0300

    main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import sqlite3
 import pandas as pd
 
-# טען את קובץ ה-CSV שהורדת
-csv_file = 'ecommerce_customer_behavior.csv'  # כאן עליך לוודא שהשם נכון
+csv_file = 'ecommerce_customer_behavior.csv'
 
-# קרא את הקובץ עם pandas
 df = pd.read_csv(csv_file)
 
-# יצירת חיבור למסד הנתונים ושמירת הנתונים בטבלה בשם 'ecomm' בתוך מסד הנתונים ecomm.db
-conn = sqlite3.connect('ecomm.db')  # שם מסד הנתונים הוא ecomm.db
+conn = sqlite3.connect('ecomm.db')
 df.to_sql('ecomm', conn, if_exists='replace', index=False)
 
-# סגירת החיבור למסד הנתונים
 conn.commit()
 conn.close()
 
